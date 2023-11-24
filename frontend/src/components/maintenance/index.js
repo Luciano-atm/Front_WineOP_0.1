b11da1bcc120bd2ea0899c935fbe92c2c889a136
luciano-tapia <luciano.tapia@alumnos.ucn.cl>
2023-11-24 10:06:21 -0300

    first commit

---
+++ frontend/src/components/maintenance/index.js
@@ -0,0 +1,186 @@
+import React, {useState, useEffect} from 'react'
+import './style.css'
+import axios from 'axios';
+import { TimePicker } from '@material-ui/pickers';
+import swal from 'sweetalert';
+import Swal from 'sweetalert2';
+
+
+export const Maintenance = ({ onClose }) => {
+    const [machines,setMachines] = useState([]);
+    const [tipo, setTipo] = useState("");
+    const [idMachine, setIdMachine] = useState(0);
+    const [diaSemana, setDiaSemana] = useState(0);
+    const [semana, setSemana] = useState(0)
+    const [horaInicio, setHoraInicio] = useState(new Date());
+    const [horaFin, setHoraFin] = useState(new Date());
+
+
+    const [mostrarFormulario, setMostrarFormulario] = useState(true);
+    const [capacidadMinima, setCapacidadMinima]= useState();
+    const [velocidadProcesado, setVelocidadProcesado]= useState();
+    const [tipoMaquina, setTipoMaquina]= useState();
+    const [cantidad, setCantidad]= useState();
+    const [capacidadMaxima, setCapacidadMaxima]= useState();
+
+    {/*useEffect(() => {
+        axios.get(`${process.env.REACT_APP_BACKEND_URL}/maquinas`).then((response) => {
+            if(response.status === 200) setMachines(response.data);
+        }).catch(() => setMachines([]))
+        
+    }, []) */}
+
+    const handlerCargarId = function(e){
+        const opcion = e.target.value;
+        setIdMachine(machines[opcion].id_maquina);
+        setTipo(machines[opcion].tipo)
+    }
+
+    const handleMantencion = async () => {
+        // Validación de valores
+        if (capacidadMinima === 0 || velocidadProcesado === 0 || cantidad === 0 || capacidadMaxima === 0 || tipoMaquina === 0 || tipoMaquina === 'Seleccione una opción') {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'Existen valores que no han sido especificados. Por favor, ingrese valores válidos.'
+            });
+            return;
+        }
+        try {
+            const response = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/setAgregarMaquina`, {
+                tipoMaquina: tipoMaquina,
+                capacidadMaxima: capacidadMaxima,
+                capacidadMinima: capacidadMinima,
+                cantidad: cantidad,
+                velocidadProcesado: velocidadProcesado
+            });
+    
+            if (response.status === 200) {
+                // Mostrar la respuesta del servidor
+                console.log(response.data);
+                
+                swal({
+                    title: "Máquina agregada correctamente",
+                    icon: "success",
+                    button: "Aceptar"
+                }).then(() => {
+                    setCapacidadMinima('');
+                    setVelocidadProcesado('');
+                    setTipoMaquina('Seleccione una opción');
+                    setCantidad('');
+                    setCapacidadMaxima('');
+                });
+            }
+        } catch (error) {
+            if (error.response && error.response.status === 400) {
+                swal({
+                    title: "No se pudo crear la mantención",
+                    icon: "error",
+                    button: "Aceptar"
+                });
+            }
+        }
+    };
+    
+
+    const handleEliminarFormulario = () => {
+        // Oculta el formulario
+        setMostrarFormulario(false);
+    };
+
+    const opcionesVelocidad = {
+        'Despalillado': ['1 hrs', '2 hrs', '3 hrs', '4 hrs', '5 hrs','6 hrs','7 hrs','8 hrs','9 hrs','10 hrs'],
+        'Prensado': ['1 hrs', '2 hrs', '3 hrs', '4 hrs', '5 hrs','6 hrs','7 hrs','8 hrs','9 hrs','10 hrs'],
+        'Pre-flotación': ['1 hrs', '2 hrs', '3 hrs', '4 hrs', '5 hrs','6 hrs','7 hrs','8 hrs','9 hrs','10 hrs'],
+        'Flotación': ['1 hrs', '2 hrs', '3 hrs', '4 hrs', '5 hrs','6 hrs','7 hrs','8 hrs','9 hrs','10 hrs'],
+        'Fermentación': ['10 días','11 días', '12 días', '13 días', '14 días', '15 días','16 días','17 días','18 días','19 días','20 días']
+    };
+    const obtenerDigito = (opcion) => {
+        return opcion.split(' ')[0]; // Obtiene la parte numérica antes del espacio
+    };
+    
+    const handleClose = () => {
+        onClose(); // Llama a la función onClose para cerrar la tabla de ingreso
+    };
+
+    
+
+
+    return (
+        <div>
+            {mostrarFormulario && (
+                <div className='main-container'>
+                
+                    <div className="container px-4 text-center">
+                
+                        <div className="row">
+                            <div className="col-6">
+                                <div className='contenedor'>
+                                    <label htmlFor="Tipomaquina">  Tarea:</label>
+                                    <select name='Tipomaquina' onChange={(event) => setTipoMaquina(event.target.value)} value={tipoMaquina}>
+                                        <option value={'Seleccione una opción'}>Seleccione una opción</option>
+                                        <option value={'Despalillado'}>Despalillado</option>
+                                        <option value={'Prensado'}>Prensado</option>
+                                        <option value={'Pre-flotación'}>Pre-flotación</option>
+                                        <option value={'Flotación'}>Flotación</option>
+                                        <option value={'Fermentación'}>Fermentación</option>
+                                    </select>
+                                </div>
+        
+                                <div className='contenedor'>
+                                    <label htmlFor="Capmax">  Capacidad Mínima:</label>
+                                    <input type='number' id='Capmax' min="1" max="100" onChange={(event) => setCapacidadMinima(event.target.value)} value={capacidadMinima}/>
+                                </div>
+
+                                <div className='contenedor'>
+                                    <label htmlFor="Cantidad">  Cantidad:</label>
+                                    <input type='number' id='Cantidad' min="1" max="100" onChange={(event) => setCantidad(event.target.value)} value={cantidad} />
+                                </div>
+                                
+                            </div>
+        
+                            <div className="col-6">
+                                <div className='contenedor'>
+                                    <label htmlFor="Velocidad">  Demora promedio:</label>
+                                    <select
+                                        id='Velocidad'
+                                        onChange={(event) => {
+                                          const velocidad = obtenerDigito(event.target.value);
+                                          setVelocidadProcesado(velocidad);
+                                        }}
+                                        value={velocidadProcesado}
+                                        disabled={tipoMaquina === 'Seleccione una opción' || !opcionesVelocidad[tipoMaquina]}
+                                      >
+                                        <option value=''>Seleccione una velocidad</option>
+                                        {tipoMaquina !== 'Seleccione una opción' && opcionesVelocidad[tipoMaquina] && (
+                                          opcionesVelocidad[tipoMaquina].map((opcion, index) => (
+                                            <option key={index} value={obtenerDigito(opcion)}>
+                                              {opcion}
+                                            </option>
+                                          ))
+                                        )}
+                                    </select>
+                                </div>
+        
+                                <div className='contenedor'>
+                                    <label htmlFor="Capmax">  Capacidad Máxima:</label>
+                                    <input type='number' id='CapacidadMaxima' min="1" max="100" onChange={(event) => setCapacidadMaxima(event.target.value)} value={capacidadMaxima}/>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-6">
+                                    <button className='button-maintenance' onClick={handleMantencion}>INGRESAR MAQUINA</button>
+                                </div>    
+                            
+                                <div className="col-6">
+                                    <button className='button-maintenance' onClick={handleClose}>ELIMINAR FORMULARIO</button>
+                                </div>    
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )} 
+        </div>   
+    );
+    
+}
---
+++ frontend/src/pages/inputs/index.js
@@ -0,0 +1,179 @@
+import React, { useState } from 'react'
+import axios from "axios";
+import { useNavigate } from 'react-router-dom'
+import { Maintenance } from '../../components/maintenance'
+import './style.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import swal from 'sweetalert';
+import FileDownload from "js-file-download";
+
+
+export const Inputs = () => {
+  const [showMaintenance, setShowMaintenance] = useState(false);
+  const [semana, setSemana] = useState(null);
+  const [archivo, setArchivo] = useState(null);
+  const [archivo_pdf, setArchivoPfd]= useState(null);
+  const [semanas, setSemanas] = useState([]);
+  const navigate = useNavigate();
+
+  const handleAdd = () => {
+    setShowMaintenance(true); // Muestra el componente cuando se presiona el botón
+  };
+  const handleClose = () => {
+    setShowMaintenance(false); // Oculta la tabla de ingreso y reactiva el botón
+  };
+
+
+
+  const subirArchivo = e => {
+    setArchivo(e);
+  }
+
+  const subirArchivoPdf = e => {
+    setArchivoPfd (e);
+  }
+
+  const insertarArchivos = async () => {
+    const f = new FormData();
+
+    for (let index = 0; index < archivo.length; index++) {
+      f.append("myfile", archivo[index]);
+    }
+
+    for (let index = 0; index < archivo_pdf.length; index++) {
+      f.append("mypdf", archivo_pdf[index]);
+    }
+
+    axios.post(`${process.env.REACT_APP_BACKEND_URL}/postFile`, f)
+    .then(response => {
+    swal({
+      title: "Archivo subido correctamente",
+      icon: "success",
+      button: "Aceptar"
+    });
+    setSemanas(response.data.semanas);
+  })
+  .catch(error => {
+    console.log(error);
+    swal({
+      title: "Error al subir el archivo",
+      text: "Ha ocurrido un error al subir el archivo.",
+      icon: "error",
+      button: "Aceptar"
+    });
+  });
+    
+    
+  }
+
+
+  const download = async (e)=>{
+    e.preventDefault();
+    axios({
+      url:`${process.env.REACT_APP_BACKEND_URL}/getFileInput`,
+      method:'GET',
+      responseType:'blob',
+    }).then((res) =>{
+      console.log(res);
+      FileDownload(res.data,"input.xlsx")
+    })
+  }
+
+
+
+  const handleSemana = async (e) => {
+    e.preventDefault();
+    if (semana) {
+      await axios.post(`${process.env.REACT_APP_BACKEND_URL}/obtenerSemana`, {
+        semana: semana
+      }).catch((error) => {
+        if (error.response.status === 400) console.log(error.response.status);
+      })
+    }
+
+  }
+
+  const handleSubmit = async (data) => {
+    navigate('/outputs/' + semana)
+  }
+
+  function imprimirMensaje() {
+    console.log("Este es un mensaje en la consola.");
+  }
+
+  const iniciarPlanificacion = async () => {
+    try {
+      const response = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/iniciarPlanificacion`);
+      console.log('Respuesta del backend:', response.data);
+    } catch (error) {
+      console.error('Error al llamar al backend:', error);
+    }
+  };
+
+
+  return (
+    <div className='background-input'>
+      <div className='box-container-input'>
+        <div className='box-top-input'>
+          <h1 className='top-text-input'>Bienvenido WineOptimize</h1>
+        </div>
+        {/*<form className='form-group' autoComplete='off' onSubmit={handleSubmit}>*/}
+          <div className='box-down-input'>
+            <div className='main-container'>
+              <div className='form'>
+                <label className='text-input-excel'><h1>Ingresar planificación enólogo </h1></label>
+                <input type='file' className='form-control' onChange={(e) => subirArchivo(e.target.files)} required></input>
+              </div>
+              <div className='form'>
+                <label className='text-input-excel'><h1>Ingresar planificación Igenieros </h1></label>
+                <input type='file' className='form-control' onChange={(e) => subirArchivoPdf(e.target.files)} required></input>
+              </div>
+              <div className="row">
+                <div className="col-6">
+                  <div className='btn-in'>
+                    <button className='button-succ' onClick={() => insertarArchivos()}>
+                      Subir
+                    </button>
+                  </div>
+                </div>   
+                <div className="col-6">    
+                  <div className='btn-out'>
+                    <button className='button-succ' onClick={(e) =>download(e)}>
+                      Descargar
+                    </button>
+                  </div> 
+                </div> 
+              </div>
+            </div>
+             {/*<div className='semana'>
+              <h1 className='semana-h1'>Seleccione la semana en la que requiere hacer la optimización:</h1>
+              <select name='semana-select' onChange={(event) => setSemana(event.target.value)} onClick={handleSemana} required>
+                <option value="">No definido</option>
+                {semanas.map(elemento => (
+                  <option value={elemento} key={elemento}>{elemento}</option>
+                ))}
+              </select>
+                </div> */}
+
+              <div className='maintenance'>
+                    <button
+                      className='button-maquina'
+                      type='button'
+                      onClick={handleAdd}
+                      disabled={showMaintenance}
+                    >
+                      Añadir Máquina
+                    </button>
+                    {showMaintenance && <Maintenance onClose={handleClose} />}
+                  </div>
+            <div className='next'>
+              <button className='button-iniciar' onClick={(e) =>iniciarPlanificacion(e)}>
+                Realizar Planificación
+              </button>
+            </div>
+          </div>
+        {/*</form>*/}
+      </div>
+    </div>
+  )
+}
